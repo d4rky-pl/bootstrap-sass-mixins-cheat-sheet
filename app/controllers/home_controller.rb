6a291a412c6e1927bd88fe2583c8f2944e127c5c
Michał Matyas <hello@higher.lv>
2014-11-28 12:13:30 +0000

    update to bootstrap v3.3.1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
     # Unfortunately bootstrap-sass team doesn't use standard master/devel branching convention
     # making it impossible to simply link to master with gem's stable version.
     # Also their tagging convention is not 1:1 with their gem versioning convention.
-    current_version_url = 'https://github.com/twbs/bootstrap-sass/tree/v3.1.1+2'
+
+    current_version_url = 'https://github.com/twbs/bootstrap-sass/tree/v3.3.1'
     json = JSON.parse(File.read(File.join(Rails.root, 'lib', 'database.json')))
     json_hash = HashWithIndifferentAccess.new(json)
     render :locals => { url: current_version_url, json: HashWithIndifferentAccess.new(json) }
